dc17152a9feaabcccd3c39fd6e56c5d63d8fc287
Benjamin Fernandes <benjamin.fernandes@mobile-devices.fr>
2012-12-21 15:45:38 +0100

    Added missing configuration methods

--- lib/cloud_connect/client/configurations.rb
+++ lib/cloud_connect/client/configurations.rb
@@ -5,9 +5,6 @@ module CloudConnect
       #
       # @param id [String] ID of the configuration
       # @return [Configuration] The configuration you requested, if it exists
-      # @example Get configuration 380342888036303061
-      #   @client = CloudConnect::Client.new(:account => 'foo', :token => 'bar')
-      #   @client.configuration("380342888036303061")
       def configuration(id, options={})
         enhance( get("configs/#{id}", options), with: ConfigurationMethods )
       end
@@ -16,21 +13,34 @@ module CloudConnect
       #
       # @param search_term [String] The term to search for
       # @return [Array] A list of configurations matching the search term
-      # @see http://wordsabout.it/mobile-devices/cloudconnect-user-documentation/cc-0005-configurations
-      # @example Search for '123' in the configurations
-      #   @client = CloudConnect::Client.new(:account => 'foor', :token => 'bar')
-      #   @client.search_configurations
       def search_configurations(search_term, options={})
         enhance( get("configs?q=#{search_term}", options), with: ConfigurationMethods )
+      end
+
+      # Create a configuration
+      #
+      # @param name [String] Name of the configuration
+      # @options options [Hash] :data JSON data of the config
+      # @options options [Array] :imeis list of assets concerned by this configuration
+      # @return [Field] Your newly created configuration
+      def create_configuration(name, options={})
+        enhance( post("configs", options.merge({:name => name})), with: ConfigurationMethods )
+      end
+
+      # Update a configuration
+      #
+      # @param id [String] Id of the configuration
+      # @options options [String] Name of the configuration
+      # @options options [Hash] :data JSON data of the config
+      # @options options [Array] :imeis list of assets concerned by this configuration
+      # @return [Field] Your newly created configuration
+      def update_configuration(id, options={})
+        enhance( put("configs/#{id}", options.merge({:name => name})), with: ConfigurationMethods )
       end
 
       # Get all configurations
       #
       # @return [Array] A list of all configurations
-      # @see http://wordsabout.it/mobile-devices/cloudconnect-user-documentation/cc-0005-configurations
-      # @example List all configurations
-      #   @client = CloudConnect::Client.new(:account => 'foo', :token => 'bar')
-      #   @client.configurations
       def configurations(options={})
         enhance( get("configs", options), with: ConfigurationMethods )
       end
